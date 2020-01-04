5dded6d7e7ca6b1710d0193d1e521db0ab9b013e
vasili sakov <vsakov@navatsoft.by>
2020-01-04 14:18:57 +0300

    first working option

--- script/script.js
+++ script/script.js
@@ -1,21 +1,106 @@
-$( document ).ready(function() {	
-	$('#myForm').submit(function(event) {
-		event.preventDefault();
-		var newURL = $("#newURL").val();
-		var nameURL = $("#nameURL").val();
-		var desc = $("#desc").val();
-		var http = new XMLHttpRequest();
-		var url = "https://script.google.com/macros/s/AKfycbwBJGTk4z_oxZIi1z_JjrP5u-VWK9q52cgqy8H1iG2G1Zt6ssPV/exec";
-		var params = "p1="+newURL+"&p2="+nameURL+"&p3="+desc;
-		http.open("GET", url + "?" + params, true);
-		http.onreadystatechange = function() {
-			if(http.readyState == 4 && http.status == 200) {
-				//alert(http.responseText);
+var myApp = "https://script.google.com/macros/s/AKfycbzux3DDKyE8zETm2_9yKXGtXcwq88Jb8BYyrXfiNa5LaqQhIr8/exec";
+//URL нашего приложения
+
+$( document ).ready(function() {
+	getLinks ();
+
+$('#myForm').submit(function(event) {
+	event.preventDefault();
+	var newURL = $("#newURL").val();
+	var nameURL = $("#nameURL").val();
+	var desc = $("#desc").val();
+	var action = "addLink";
+	var xhr = new XMLHttpRequest();
+	var body = 'p1=' + encodeURIComponent(newURL) + '&p2=' + encodeURIComponent(nameURL)  + '&p3=' + encodeURIComponent(desc) + '&action=' + encodeURIComponent(action);
+	xhr.open("POST", myApp, true);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+//        	alert(xhr.response);
+        	document.getElementById("myForm").reset();//сбрасываем форму
+	//		$('#addTaskModal').modal('hide');//скрываем модалку
+			getLinks ();//обновляем список задач
 			}
-		}
-		http.send(null);
-	});
+    };
+	try { xhr.send(body);} catch (err) {console.log(err) }
+});
 });
 
+function getLinks () {
+    var action = "getLink";
+    var url = myApp+"?action="+action
+
+    //подготавливаем и выполняем GET запрос
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+       //в случае успеха преобразуем полученный ответ в JSON и передаем отдельной функции, которая сформирует нам таблицу
+        	var data = JSON.parse(xhr.response);
+        	linkTable (data);
+        }
+    };
+    try { xhr.send(); } catch (err) {console.log(err) }
+}
+
+	//функция, которая обработает данные, полученные при выполнении запроса и сформирует из них таблицу
+function linkTable (data) {
+	$('.tblbody').html( function () {	
+		var td = '';
+		for ( i = 0; i < data.length; i++ ) {
+			td +=   '<tr>'+
+					'<td>'+'<a href="https://'+ data[i][1] +'"'+ 'class="URL" target="_blank">'+ 
+					data[i][1] +
+					'</td>'+
+					'</tr>'+
+					'<tr>'+
+					'<td>'+data[i][2]+'</td>'+
+					'</tr>'+
+					'<tr>'+
+					'<td>'+data[i][3]+'</td>'+
+					'</tr>'+
+					'<tr>'+
+					'<td hidden="true" >'+data[i][0]+'</td>'+
+					'<td><button type="button" class="btn btn-link" onclick="deleteLink(\''+data[i][0]+'\')">Удалить</button></td>'+
+					'</tr>';
+		}
+		//собираем и возвращаем готовую таблицу
+		return '<table class="table"><tbody>'+td+'</tbody></table>'
+		
+	})
+}
 
 
+
+
+
+function deleteLink (link) {
+	var action = "deleteLink";
+	var xhr = new XMLHttpRequest();
+	var body = 'link=' + encodeURIComponent(link) + '&action=' + encodeURIComponent(action);
+	xhr.open("POST", myApp, true);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+//        	alert(xhr.response);
+		getLinks ();//обновляем список задач
+        }
+    };
+	try { xhr.send(body);} catch (err) {console.log(err) }
+}	
+	
+	
+$( document ).ready(function(){
+
+		$('.catalog').click(function() {
+  $('.tblbody').toggle();
+})
+	$('.catalog2').click(function() {
+  $('.tblbody2').toggle();
+})
+})
+
+	
+	
+	
---
+++ scriptcodeWEB.js
@@ -0,0 +1,61 @@
+// JavaScript Document
+//Получаем доступ к таблице по ссылке с уникальным идентификатором
+var LinkCollector = SpreadsheetApp.openById("12Z9OGKaxJaTBoni9dDCc2bWH54m4URNz60RTBNXfqYo");
+//Получаем доступ к странице по ее имени
+var Link1 = LinkCollector.getSheetByName("Link1");
+//Стандартная функция Google Apps Script для прослушивания входящих запросов, отправленных методом POST
+function doPost (e) {
+  var operation = e.parameter.action;//получаем параметр "action"
+  
+  switch (operation) {
+      case "deleteLink": return deleteLink (e);
+       case "addLink": return addLink (e);
+  }
+
+}
+
+//Стандартная функция Google Apps Script для прослушивания входящих запросов, отправленных методом GET
+function doGet (e) {
+  var operation = e.parameter.action;//получаем параметр "action"
+  
+  switch (operation) { 
+   
+    case "getLink": return getLink ();
+  }
+
+}
+//Функция, отвечающая за добавление новых задач
+function addLink (e) {
+
+   var dateTime = Utilities.formatDate(new Date(), 'America/New_York', 'MMMM dd, yyyy HH:mm:ss Z');
+  //определяем дату в нужном формате и часовом поясе
+  var newURL = e.parameter.p1;//получаем название задачи в переданном параметре
+   var nameURL = e.parameter.p2;
+   var desc = e.parameter.p3;
+  
+  Link1.appendRow([dateTime,newURL,nameURL,desc]); //обращаемся к нашей странице “Link1” определяем крайнюю свободную строку и вставляем полученные значения.
+  return ContentService.createTextOutput('Link успешно добавлен!');//возвращает в ответ текстовое сообщение об успехе
+}
+
+//Функция, отвечающая за получение строк и отправку данных клиенту
+function getLink () {
+  var lastrow = Link1.getLastRow();//получаем номер последней строки в таблице
+  var data = Link1.getRange("A1:D" + lastrow).getValues();//получаем массив нужных колонок
+  return ContentService.createTextOutput(JSON.stringify(data)).setMimeType(ContentService.MimeType.JSON);//возвращает в ответ полученные данные в JSON формате
+}
+
+//Функция, отвечающая за удаление задач
+function deleteLink (e) {
+  var link = e.parameter.link;//получаем название задачи в переданном параметре
+  var lastrow = Link1.getLastRow();//получаем номер последней строки в таблице
+  var array = Link1.getRange("A1:A" + lastrow).getValues();//получаем массив указанных ячеек колонки, в которой будем искать соответствие
+  
+  for (var i = 0; i <= array.length; i++) {
+    if (array[i] == link) {//если элемент соответствует искомому в массиве, то...
+      Link1.deleteRow(i+1);//обращаемся к нашей странице “Link1” и удаляем строку, в которой  было найдено совпадение. Прибавляем единичку, т.к. это был массив и у него нумерация идет с нуля...
+      break;//завершаем цикл, т.к. мы нашли что искали и сделали, что хотели
+    }
+  }
+    
+  return ContentService.createTextOutput("Задача успешно удалена!");
+}
