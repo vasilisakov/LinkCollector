44170d11dd253d3c56e89a1ac3ae55661e0ceaeb
vasili sakov <vsakov@navatsoft.by>
2020-01-16 16:26:15 +0300

    new correct

--- script/script.js
+++ script/script.js
@@ -1,29 +1,31 @@
-var myApp = "https://script.google.com/macros/s/AKfycbwU3WyMG53pJjay-QXJfDBCGXd9Jovu_aar2mdYzT7fpGM_Uno/exec";
+var myApp = "https://script.google.com/macros/s/AKfycbzcsJ0BcQJa1c6-3Dwsqeie7lgrLkhRfC1DnXbI2g4v38g4LoQ/exec";
 //URL нашего приложения
 
 $( document ).ready(function() {
 	getLinks ();
 
-$('#myForm').submit(function(event) {
-	event.preventDefault();
-	var newURL = $("#newURL").val();
-	var nameURL = $("#nameURL").val();
-	var desc = $("#desc").val();
-	var action = "addLink";
-	var xhr = new XMLHttpRequest();
-	var body = 'p1=' + encodeURIComponent(newURL) + '&p2=' + encodeURIComponent(nameURL)  + '&p3=' + encodeURIComponent(desc) + '&action=' + encodeURIComponent(action);
-	xhr.open("POST", myApp, true);
-	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-//        	alert(xhr.response);
-        	document.getElementById("myForm").reset();//сбрасываем форму
-	//		$('#addTaskModal').modal('hide');//скрываем модалку
-			getLinks ();//обновляем список задач
-			}
-    };
-	try { xhr.send(body);} catch (err) {console.log(err) }
-});
+	$('#myForm').submit(function(event) {
+		event.preventDefault();
+		var newURL = $("#newURL").val();
+		var nameURL = $("#nameURL").val();
+		var desc = $("#desc").val();
+		var action = "addLink";
+		var xhr = new XMLHttpRequest();
+		var body = 'p1=' + encodeURIComponent(newURL) + '&p2=' + encodeURIComponent(nameURL)  + '&p3=' + encodeURIComponent(desc) + '&action=' + encodeURIComponent(action);
+		xhr.open("POST", myApp, true);
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				document.getElementById("myForm").reset();//сбрасываем форму
+				$("#openModal").hide();//скрываем модалку 
+				getLinks ();//обновляем список задач
+				}
+			};
+			
+		try { xhr.send(body);} 
+		catch (err) {console.log(err) }
+		
+	});
 });
 
 function getLinks () {
@@ -36,19 +38,20 @@ function getLinks () {
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
-       //в случае успеха преобразуем полученный ответ в JSON и передаем отдельной функции, которая сформирует нам таблицу
+			//в случае успеха преобразуем полученный ответ в JSON и передаем отдельной функции, которая сформирует нам таблицу
         	var data = JSON.parse(xhr.response);
         	linkTable (data);
         }
-    };
-    try { xhr.send(); } catch (err) {console.log(err) }
-}
+	};
+		try { xhr.send(); } 
+		catch (err) {console.log(err) }
+};
 
 	//функция, которая обработает данные, полученные при выполнении запроса и сформирует из них таблицу
 function linkTable (data) {
 	$('.tblbody').html( function () {	
 		var td = '';
-		for ( i = 0; i < data.length; i++ ) {
+		for ( i = 0; i < data.length; i++) {
 			td +=   '<tr>'+
 					'<td>'+'<a href="https://'+ data[i][1] +'"'+ 'class="URL" target="_blank">'+ 
 					data[i][1] +
@@ -62,7 +65,8 @@ function linkTable (data) {
 					'</tr>'+
 					'<tr>'+
 					'<td hidden="true" >'+data[i][0]+'</td>'+
-				'<td><a href="#openModal2"><button type="button" class="btn btn-link">изменить</button></a></td>'+
+				'<td><button type="button" id="btn_modal_window" onclick="fillingModal(\''+data[i][0]+'\')">Изменить</button></td>'+
+				
 					'<td><button type="button" class="btn btn-link" onclick="deleteLink(\''+data[i][0]+'\')">Удалить</button></td>'+
 					'</tr>';
 		}
@@ -71,9 +75,6 @@ function linkTable (data) {
 		
 	})
 }
-
-
-
 
 
 function deleteLink (link) {
@@ -89,9 +90,72 @@ function deleteLink (link) {
         }
     };
 	try { xhr.send(body);} catch (err) {console.log(err) }
-}	
+}
 
 
+function fillingModal(link) {
+	
+	var action = "fillingModal";
+	var xhr = new XMLHttpRequest();
+    var body = 'link=' + encodeURIComponent(link) + '&action=' + encodeURIComponent(action);
+	xhr.open("POST", myApp, true);
+	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+		   //в случае успеха преобразуем полученный ответ в JSON и передаем отдельной функции
+		/* 	alert(xhr.response); */
+			var data = JSON.parse(xhr.response);
+			updateLinkModal(data);
+		}
+	}
+	try { xhr.send(body);} catch (err) {console.log(err) }
+
+}
+
+
+function updateLinkModal(data) {
+	
+	var title = 'Редактировать данные';
+   for ( i = 0; i < data.length; i++ ) {
+			var input = '<div class="form-group">'+
+			                '<textarea name="text" id="newLink">'+ data[i][1]+'</textarea>' +
+							'<textarea type="text" id="newName">' +data[i][2]+'</textarea>' +
+							'<textarea type="text" id="newDesc">' + data[i][3]+'</textarea>' +
+			            '</div>';
+				break;
+   }
+	
+	var form = '<form id="updateLinkForm" onsubmit="return true;">'+input+'</form>';
+		var buttons = '<button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>'+
+            	      '<button type="button" class="btn btn-success" onclick="updateLink(\''+data[i][0]+'\')">Сохранить</button>';
+
+	$('#updateLinkModal .modal-header .modal-title').html(title);
+	$('#updateLinkModal .modal-body').html(form);
+	$('#updateLinkModal .modal-footer').html(buttons);
+	$("#updateLinkModal").show();
+
+}
+
+
+function updateLink(link) {
+		var action = "updateLink";
+		var newLink = $('#newLink').val();
+		var newName = $('#newName').val();
+		var newDesc = $('#newDesc').val();
+		var xhr = new XMLHttpRequest();
+		var body = 'link=' + encodeURIComponent(link) + '&newLink=' + encodeURIComponent(newLink) + '&newName=' + encodeURIComponent(newName)+ '&newDesc=' + encodeURIComponent(newDesc) + 
+		'&action=' + encodeURIComponent(action);
+		xhr.open("POST", myApp, true);
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				$("#updateLinkModal").hide();
+			getLinks ();//обновляем список задач
+			}
+		};
+		try { xhr.send(body);} catch (err) {console.log(err) }
+
+	}
 	
 $( document ).ready(function(){
 
@@ -103,6 +167,3 @@ $( document ).ready(function(){
 })
 })
 
-	
-	
-	
