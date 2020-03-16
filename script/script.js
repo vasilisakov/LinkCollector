db8c435d8e1bf6eabdb765721d83a173cd5675d7
vasili sakov <vsakov@navatsoft.by>
2020-03-16 09:07:04 +0300

    16.03.2020

--- script/script.js
+++ script/script.js
@@ -1,23 +1,30 @@
-var myApp = "https://script.google.com/macros/s/AKfycbzcsJ0BcQJa1c6-3Dwsqeie7lgrLkhRfC1DnXbI2g4v38g4LoQ/exec";
+var myApp = "https://script.google.com/macros/s/AKfycbwmtjC6DDc5AvOMIsjvCXhn8MQie2CTnBQBtU9SU4YKcphJ2Dkt/exec";
 //URL нашего приложения
 
-$( document ).ready(function() {
-	getLinks ();
 
-	$('#myForm').submit(function(event) {
+$(document).ready(function() {
+	getLinks ();
+	
+	
+	$(document).on('click','.catalog', function(){
+		$(this).next().toggle();
+});
+	
+	$('[name != myForm]').submit(function(event) {
 		event.preventDefault();
-		var newURL = $("#newURL").val();
-		var nameURL = $("#nameURL").val();
-		var desc = $("#desc").val();
+		var newURL = $('[name = newURL]').val();
+		var nameURL = $('[name = nameURL]').val();
+		var desc = $('[name = desc]').val();
 		var action = "addLink";
+		var hidden = $('[name = hidden]').val();
 		var xhr = new XMLHttpRequest();
-		var body = 'p1=' + encodeURIComponent(newURL) + '&p2=' + encodeURIComponent(nameURL)  + '&p3=' + encodeURIComponent(desc) + '&action=' + encodeURIComponent(action);
+		var body = 'p1=' + encodeURIComponent(newURL) + '&p2=' + encodeURIComponent(nameURL)  + '&p3=' + encodeURIComponent(desc) + '&p4=' + encodeURIComponent(hidden) + '&action=' + encodeURIComponent(action);
 		xhr.open("POST", myApp, true);
 		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState == 4 && xhr.status == 200) {
-				document.getElementById("myForm").reset();//сбрасываем форму
-				$("#openModal").hide();//скрываем модалку 
+				document.getElementsByName("myForm").reset();//сбрасываем форму
+				$("#openModal").closest();//скрываем модалку 
 				getLinks ();//обновляем список задач
 				}
 			};
@@ -26,7 +33,58 @@ $( document ).ready(function() {
 		catch (err) {console.log(err) }
 		
 	});
+	
+	
+	
+	
+	
+	
+$('#Catalog').submit(function(event) {
+		event.preventDefault();
+		var nameCatalog = $("#nameCatalog").val();
+		var action = "newCatalog";
+		var xhr = new XMLHttpRequest();
+		var body = 'p1=' + encodeURIComponent(nameCatalog) + '&action=' + encodeURIComponent(action);
+		xhr.open("POST", myApp, true);
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				document.getElementById("Catalog").reset();//сбрасываем форму
+				$("#openModal2").hide();//скрываем модалку 
+				alert("OK");
+				getLinks ();//обновляем список задач
+				}
+			};
+			
+		try { xhr.send(body);} 
+		catch (err) {console.log(err) }
+		
+	});
+
 });
+
+
+function LinkModal(data) {
+	
+	var title = 'Окно ввода данных';
+   
+			var input = '<div class="form-group">'+
+			                '<input type="text" id="newURL" placeholder="Add new URL">' +
+							'<input type="text" id="nameURL" placeholder="Add name URL">' +
+							'<input type="text" id="desc" placeholder="Add description">' +
+							'<input type="hidden" id="hidden" value="' +data +'">' +
+			            '</div>';
+			
+	var form = '<form id="LinkForm" onsubmit="return true;">'+input+'</form>';
+		var buttons = '<button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>';
+            	      
+
+	$('#LinkModal .modal-header .modal-title').html(title);
+	$('#LinkModal .modal-body').html(form);
+	$('#LinkModal .modal-footer').html(buttons);
+	$("#LinkModal").show();
+
+}
 
 function getLinks () {
     var action = "getLink";
@@ -39,8 +97,8 @@ function getLinks () {
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
 			//в случае успеха преобразуем полученный ответ в JSON и передаем отдельной функции, которая сформирует нам таблицу
-        	var data = JSON.parse(xhr.response);
-        	linkTable (data);
+        	var linki = JSON.parse(xhr.response);
+        	linkTable (linki);
         }
 	};
 		try { xhr.send(); } 
@@ -48,33 +106,77 @@ function getLinks () {
 };
 
 	//функция, которая обработает данные, полученные при выполнении запроса и сформирует из них таблицу
-function linkTable (data) {
-	$('.tblbody').html( function () {	
+
+function linkTable (linki) {
+	let Catalog = document.getElementById('catalogs');
+	
+	linki.forEach(function (elem) {
+		let container = document.createElement('div');
+		let table = document.createElement('div');
+		let application = document.createElement('div');
+		let button = document.createElement('button');
+
+		Catalog.appendChild(container);
+
+		container.setAttribute('class', 'container');
+        container.setAttribute('name', `${elem.name}`);
+	    container.innerHTML = `<h2 class="catalog">${elem.name}</h2>`;
+		application.setAttribute('style', 'display: none');
+		container.append(application);
+		button.setAttribute('onclick', `LinkModal("${elem.name}");`);
+		
+		button.innerHTML = `Окно ввода данных`;
+		application.append(button);
+		table.setAttribute('class', 'tblbody');
+		table.setAttribute('id', `${elem.name}`);
+		application.append(table);
+		Catalog.appendChild(container);
+
+		
+		
+		$(`#${elem.name}`).html( function () {	
 		var td = '';
-		for ( i = 0; i < data.length; i++) {
+		for ( i = 0; i < elem.data.length; i++) {
 			td +=   '<tr>'+
-					'<td>'+'<a href="https://'+ data[i][1] +'"'+ 'class="URL" target="_blank">'+ 
-					data[i][1] +
+					'<td>'+'<a href="https://'+ elem.data[i][1] +'"'+ 'class="URL" target="_blank">'+ 
+					elem.data[i][1] +
 					'</td>'+
 					'</tr>'+
 					'<tr>'+
-					'<td>'+data[i][2]+'</td>'+
+					'<td>'+elem.data[i][2]+'</td>'+
 					'</tr>'+
 					'<tr>'+
-					'<td>'+data[i][3]+'</td>'+
+					'<td>'+elem.data[i][3]+'</td>'+
 					'</tr>'+
 					'<tr>'+
-					'<td hidden="true" >'+data[i][0]+'</td>'+
-				'<td><button type="button" id="btn_modal_window" onclick="fillingModal(\''+data[i][0]+'\')">Изменить</button></td>'+
-				
-					'<td><button type="button" class="btn btn-link" onclick="deleteLink(\''+data[i][0]+'\')">Удалить</button></td>'+
+					'<td hidden="true" >'+elem.data[i][0]+'</td>'+
+				'<td><button  id="btn_modal_window" onclick="fillingModal(\''+elem.data[i][0]+'\')">Изменить</button></td>'+
+					'<td><button type="button" class="btn btn-link" onclick="deleteLink(\''+elem.data[i][0]+'\')">Удалить</button></td>'+
 					'</tr>';
 		}
 		//собираем и возвращаем готовую таблицу
 		return '<table class="table"><tbody>'+td+'</tbody></table>'
 		
 	})
-}
+	
+	
+	});
+		
+		
+};
+
+	
+
+
+
+
+
+
+
+
+
+
+
 
 
 function deleteLink (link) {
@@ -156,14 +258,25 @@ function updateLink(link) {
 		try { xhr.send(body);} catch (err) {console.log(err) }
 
 	}
-	
-$( document ).ready(function(){
 
-		$('.catalog').click(function() {
-  $('.tblbody').toggle();
-})
-	$('.catalog2').click(function() {
-  $('.tblbody2').toggle();
-})
-})
+	function newCatalog(){
+		var action = "newCatalog";
+    var url = myApp+"?action="+action
+
+    //подготавливаем и выполняем GET запрос
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+			//в случае успеха преобразуем полученный ответ в JSON и передаем отдельной функции, которая сформирует нам таблицу
+        	var data = JSON.parse(xhr.response);
+        	alert(xhr.response);
+        }
+	};
+		try { xhr.send(); } 
+		catch (err) {console.log(err) }
+};
+		
+  
 
